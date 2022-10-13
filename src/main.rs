e2bc3fa8a6dcf9c8cbc6b46f5e18d7b4e553451b
Hikari Fireball <7264271+hikari-fireball@users.noreply.github.com>
2022-10-13 20:13:41 +0200

    brute force

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ const BLOCK_SIZE: usize = 2;
 const BOARD_SIZE: usize = BLOCK_SIZE.pow(2);
 const CELL_DOMAIN: RangeInclusive<usize> = 1..=BOARD_SIZE;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Board {
     cells: Vec<Vec<HashSet<usize>>>,
 }
@@ -68,6 +68,41 @@ impl Board {
 
         true
     }
+
+    fn most_constrained_variable(&self) -> Option<(usize, usize)> {
+        let domain_size = CELL_DOMAIN.collect::<Vec<usize>>().len();
+        let (mut x, mut y, mut min) = (0, 0, domain_size + 1);
+        for line in 0..BOARD_SIZE {
+            for column in 0..BOARD_SIZE {
+                if self.cells[line][column].len() < min && self.cells[line][column].len() > 1 {
+                    y = line;
+                    x = column;
+                    min = self.cells[line][column].len();
+                }
+            }
+        }
+        if min <= domain_size {
+            Some((y, x))
+        } else {
+            None
+        }
+    }
+
+    fn offspring(&self) -> Vec<Board> {
+        match self.most_constrained_variable() {
+            Some((line, column)) => {
+                let mut children: Vec<Board> = vec![];
+                for value in self.cells[line][column].clone() {
+                    let mut child = self.clone();
+                    child.cells[line][column].clear();
+                    child.cells[line][column].insert(value);
+                    children.push(child);
+                }
+                children
+            }
+            None => Vec::new(),
+        }
+    }
 }
 
 fn main() {
@@ -75,8 +110,6 @@ fn main() {
         "sudoku {:?}/{:?} ({:?})",
         BLOCK_SIZE, BOARD_SIZE, CELL_DOMAIN
     );
-    // let mut stack: Vec<Board> = vec![Board::new()];
-    // println!("{:?}", stack);
 
     let mut root = Board::new();
     root.cells[0][0] = HashSet::from([1]);
@@ -95,5 +128,22 @@ fn main() {
     root.cells[3][1] = HashSet::from([3]);
     root.cells[3][2] = HashSet::from([4]);
     root.cells[3][3] = HashSet::from([1]);
-    println!("{:?}", root.is_valid());
+    assert!(root.is_valid());
+
+    let root = Board::new();
+    let mut stack: Vec<Board> = vec![root];
+    let mut i = 0;
+    while let Some(parent) = stack.pop() {
+        if i % 1000000 == 0 {
+            println!("{:?}", i as f32 / 4294967296.0);
+        }
+        for child in parent.offspring() {
+            if child.is_valid() {
+                println!("{:?}", child);
+            } else {
+                stack.push(child);
+            }
+        }
+        i = i + 1;
+    }
 }
