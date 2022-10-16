9e722364ea57f73591a29b6adc3377edb6b42875
Hikari Fireball <7264271+hikari-fireball@users.noreply.github.com>
2022-10-16 12:54:03 +0200

    move generic code to separate module
    * move generic code to separate module
    * modified sudoku types

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,14 @@
-use std::collections::HashMap;
+mod csp;
+
 use std::collections::HashSet;
 use std::fmt;
 use std::hash::Hash;
 use std::ops::RangeInclusive;
 
-// TODO export State related to it's one namespace
-
-#[derive(Debug, Clone)]
-struct State<V, D> {
-    domains: HashMap<V, HashSet<D>>,
-}
-
-impl<V, D> State<V, D>
-where
-    State<V, D>: Searchable<V, D> + Clone,
-    V: Eq + Hash + Copy,
-    D: Eq + Hash + Copy,
-{
-    fn new(variables: &[V], domain: &HashSet<D>) -> State<V, D> {
-        State {
-            domains: variables.iter().map(|v| (*v, domain.clone())).collect(),
-        }
-    }
-
-    fn most_constrained_variable(&self) -> Option<&V> {
-        self.domains
-            .iter()
-            .filter(|(_, v)| v.len() > 1)
-            .min_by(|(_, v1), (_, v2)| v1.len().cmp(&v2.len()))
-            .map(|(k, _)| k)
-    }
-
-    fn offspring(&self) -> Vec<State<V, D>> {
-        match self.most_constrained_variable() {
-            Some(position) => {
-                let mut children: Vec<State<V, D>> = vec![];
-                for value in self.domains.get(position).unwrap() {
-                    let mut child = self.clone();
-                    let cell = child.domains.get_mut(position).unwrap();
-                    cell.clear();
-                    cell.insert(*value);
-                    if child.simplify(position, *value) {
-                        children.push(child);
-                    }
-                }
-                children
-            }
-            None => Vec::new(),
-        }
-    }
-}
-
-pub trait Searchable<V, D> {
-    fn check_constraints(&self) -> bool;
-    fn simplify(&mut self, variable: &V, value: D) -> bool; // TODO modify to return a Result
-}
-
-// --- //
-
 #[derive(Clone, Eq, Hash, PartialEq, Copy)]
 struct Position {
-    line: u32,
-    column: u32,
+    line: usize,
+    column: usize,
 }
 
 impl fmt::Debug for Position {
@@ -71,12 +18,13 @@ impl fmt::Debug for Position {
 }
 
 // TODO move these constants somewhrere where they can be accessed by the Sudoku code
-const BLOCK_SIZE: u32 = 3; // TODO make usize
-const BOARD_SIZE: u32 = BLOCK_SIZE.pow(2); // TODO make usize
-const CELL_DOMAIN: RangeInclusive<SudokuDomainValue> = 1..=BOARD_SIZE;
+const BLOCK_SIZE: usize = 2;
+const BOARD_SIZE: usize = BLOCK_SIZE.pow(2);
+const CELL_DOMAIN: RangeInclusive<SudokuDomainValue> = 1..=(BOARD_SIZE as u32);
 
 type SudokuDomainValue = u32;
-type Sudoku = State<Position, SudokuDomainValue>;
+type Sudoku = csp::State<Position, SudokuDomainValue>;
+
 impl Sudoku {
     fn adjacent_mut(
         &mut self,
@@ -99,7 +47,8 @@ impl Sudoku {
             .collect::<Vec<(&Position, &mut HashSet<SudokuDomainValue>)>>()
     }
 }
-impl Searchable<Position, SudokuDomainValue> for Sudoku {
+
+impl csp::Searchable<Position, SudokuDomainValue> for Sudoku {
     fn check_constraints(&self) -> bool {
         // TODO: imporove this code somehow
         for line in 0..BOARD_SIZE {
@@ -116,7 +65,6 @@ impl Searchable<Position, SudokuDomainValue> for Sudoku {
                 present.insert(*single);
             }
         }
-
         for column in 0..BOARD_SIZE {
             let mut present: HashSet<SudokuDomainValue> = HashSet::new();
             for line in 0..BOARD_SIZE {
@@ -131,7 +79,6 @@ impl Searchable<Position, SudokuDomainValue> for Sudoku {
                 present.insert(*single);
             }
         }
-
         for line_offset in (0..BOARD_SIZE).step_by(BLOCK_SIZE as usize) {
             for column_offset in (0..BOARD_SIZE).step_by(BLOCK_SIZE as usize) {
                 let mut present: HashSet<SudokuDomainValue> = HashSet::new();
@@ -150,64 +97,50 @@ impl Searchable<Position, SudokuDomainValue> for Sudoku {
                 }
             }
         }
-
         true
     }
 
-    fn simplify(&mut self, position: &Position, value: SudokuDomainValue) -> bool {
+    fn simplify(&mut self, position: &Position, value: SudokuDomainValue) -> Result<(), ()> {
         // TODO: [IMPORTANT] call simplify again when a cell becomes determined: if cell.remove() and cell.len() == 1
-        // TODO: return result instead
         for (_, domain) in self.adjacent_mut(position) {
             domain.remove(&value);
             if domain.is_empty() {
-                return false;
+                return Err(());
             }
         }
-        true
+        Ok(())
     }
 }
 
-type Nqueens = State<Position, bool>;
-impl Searchable<Position, bool> for Nqueens {
+type Nqueens = csp::State<Position, bool>;
+impl csp::Searchable<Position, bool> for Nqueens {
     fn check_constraints(&self) -> bool {
-        println!("nqueens");
         true
     }
 
-    fn simplify(&mut self, _variable: &Position, _value: bool) -> bool {
-        false
+    fn simplify(&mut self, _variable: &Position, _value: bool) -> Result<(), ()> {
+        Ok(())
     }
 }
-
-//======
 
 fn main() {
-    // TODO move this code out and leave only initialization and a function call
-
-    let nqueens: Nqueens =
-        Nqueens::new(&[Position { line: 0, column: 0 }], &HashSet::<bool>::new());
-    nqueens.check_constraints();
-    nqueens.offspring();
+    println!("nqueens (dummy)",);
+    let nqueens: Nqueens = Nqueens::new(
+        &[Position { line: 0, column: 0 }],
+        &HashSet::<bool>::from([true, false]),
+    );
+    csp::find_all(nqueens);
 
     println!(
         "sudoku {:?}/{:?} ({:?})",
         BLOCK_SIZE, BOARD_SIZE, CELL_DOMAIN
     );
 
-    let variables: Vec<Position> = (0..BOARD_SIZE)
-        .flat_map(|j| (0..BOARD_SIZE).map(move |k| Position { line: j, column: k }))
-        .collect();
-    let domain: HashSet<SudokuDomainValue> = CELL_DOMAIN.collect::<HashSet<SudokuDomainValue>>();
-
-    let root = Sudoku::new(&variables, &domain);
-    let mut stack: Vec<State<Position, SudokuDomainValue>> = vec![root];
-    while let Some(parent) = stack.pop() {
-        for child in parent.offspring() {
-            if child.check_constraints() {
-                println!("{:?}", child);
-            } else {
-                stack.push(child);
-            }
-        }
-    }
+    let sudoku = Sudoku::new(
+        &(0..BOARD_SIZE)
+            .flat_map(|j| (0..BOARD_SIZE).map(move |k| Position { line: j, column: k }))
+            .collect::<Vec<Position>>(),
+        &CELL_DOMAIN.collect(),
+    );
+    csp::find_all(sudoku);
 }
